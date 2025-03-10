7c866cbc36d37228e7426ac9ddfa76cdbd36ae23
Volodya <andrij.chypra@gmail.com>
2025-03-11 01:57:09 +0200

    Refactored error with serilog

---
+++ PumpItUP/Models/IndexModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PumpItUp.Models
+{
+    public class IndexModel(ILogger<IndexModel> logger) : PageModel
+    {
+        public void OnGet()
+        {
+            try
+            {
+                logger.LogInformation("Test log");
+            }catch (Exception ex)
+            {
+                logger.LogError(message: ex.Message, ex);
+                throw;
+            }
+        }
+    }
+}
