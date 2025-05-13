a70d01bbd17a0730ec14ec42f9ce5927fae3ea5c
Marined01 <mariana.hayovska@gmail.com>
2025-05-13 13:43:52 +0300

    ui_changes

--- PumpItUp.BLL/obj/Debug/net8.0/PumpItUp.BLL.AssemblyInfo.cs
+++ PumpItUp.BLL/obj/Debug/net8.0/PumpItUp.BLL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PumpItUp.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaebfa0921d2927658edb44ced863afdb3ee5420")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac600eda2649882873628e86bfcefd8729a817df")]
 [assembly: System.Reflection.AssemblyProductAttribute("PumpItUp.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PumpItUp.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
